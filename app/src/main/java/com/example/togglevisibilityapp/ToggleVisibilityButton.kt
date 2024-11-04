749d333922393f7101acf8e8109f227c84ee0f19
Nieve2205 <ashleepre2022@gmail.com>
2024-11-03 19:21:12 -0500

    Animate Color with tween

--- app/src/main/java/com/example/togglevisibilityapp/ToggleVisibilityButton.kt
+++ app/src/main/java/com/example/togglevisibilityapp/ToggleVisibilityButton.kt
@@ -1,25 +1,27 @@
 package com.example.togglevisibilityapp
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ToggleVisibilityButton() {
     var isVisible by remember { mutableStateOf(false) }
+    var isBlue by remember { mutableStateOf(true) }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Button(onClick = { isVisible = !isVisible }) {
@@ -31,11 +33,20 @@ fun ToggleVisibilityButton() {
             enter = fadeIn(),
             exit = fadeOut()
         ) {
+            val color by animateColorAsState(
+                targetValue = if (isBlue) Color.Blue else Color.Green,
+                animationSpec = tween(durationMillis = 500)
+            )
+
             Box(
                 modifier = Modifier
                     .size(100.dp)
-                    .background(Color.Red)
+                    .background(color)
             )
+        }
+
+        Button(onClick = { isBlue = !isBlue }) {
+            Text(text = if (isBlue) "Cambiar a Verde" else "Cambiar a Azul")
         }
     }
 }
@@ -45,4 +56,3 @@ fun ToggleVisibilityButton() {
 fun PreviewToggleVisibilityButton() {
     ToggleVisibilityButton()
 }
-
